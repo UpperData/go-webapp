fcb0fa30b0ac6205a24cad1590d8e89b3728525f
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2023-12-12 16:15:20 -0400

    urlbase fix

--- src/configs/AppConfig.js
+++ src/configs/AppConfig.js
@@ -1,5 +1,5 @@
-export const APP_NAME           = 'CEMA';
-export const API_BASE_URL       = "http://135.148.68.155:4094/"
+export const APP_NAME           = 'RepuestosGo';
+export const API_BASE_URL       = "https://mls-r.vps.co.ve"
 
 export const AUTH_PREFIX_PATH   = '/';
 export const APP_PREFIX_PATH    = '/dashboard';
